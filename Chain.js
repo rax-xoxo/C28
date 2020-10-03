4a903b09fc0cc52bd4da959cf0ddfb870d083b99
Rashi Sharma <69389235+Rashi260@users.noreply.github.com>
2020-10-03 17:55:46 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,26 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            pointB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.chain.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    release(){
+        this.chain.bodyA = null;
+    }
+    
+}
